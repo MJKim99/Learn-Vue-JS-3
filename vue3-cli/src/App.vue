36b4f21ea97649dd888ca997a305927acca7a63b
김민지 <9mjkim@gmail.com>
2024-07-03 15:10:24 +0900

    vue 3 시작하기

---
+++ vue3-cli/src/App.vue
@@ -0,0 +1,74 @@
+<template>
+  <AppHeader 
+    :appTitle="message"
+    v-on:change="changeMessage">
+  </AppHeader>
+  <!-- <div>
+    {{ message }}
+  </div>
+  <button @click="showAlert">경고</button> -->
+</template>
+
+<script>
+  //import 컴포넌트 이름 from './컴포넌트 경로'
+  import AppHeader from './components/AppHeader.vue'
+
+  export default {
+    components: {
+      //'컴포넌트 이름': 컴포넌트 내용
+      // 'app-header': AppHeader
+      // 'AppHeader': AppHeader
+      // AppHeader: AppHeader
+      AppHeader
+    },
+    data() {
+      return {
+        message: '앱 헤더 컴포넌트'
+      }
+    },
+    methods: {
+      changeMessage() {
+        this.message = '변경됨';
+      },
+      showAlert() {
+        alert('Hello')
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
+
+<!-- HTML
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Welcome to Your Vue.js App"/>
+</template>
+
+<!- JavaScript
+<script>
+// import 라이브러리 from '라이브러리 이름'
+// import 파일 이름 from './파일 경로'
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<!- CSS
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style> -->
---
+++ vue3-form/src/App.vue
@@ -0,0 +1,63 @@
+<template>
+  <form action="" @submit.prevent="submitForm">
+    <div>
+      <label for="userId">ID: </label>
+      <input type="text" name="userId" id="userId" v-model="username">
+    </div>
+    <div>
+      <label for="password">PW: </label>
+      <input type="text" name="password" id="password" v-model="password">
+    </div>
+    <button type="submit">로그인</button>
+  </form>
+</template>
+
+<script>
+  import axios from 'axios'
+  import { ref } from 'vue'
+
+  export default {
+    setup() { // composition API
+      // data
+      const username = ref('');
+      const password = ref('');
+
+      // methods
+      const submitForm = () => {
+        axios.post('https://jsonplaceholder.typicode.com/users', {
+          username: username.value,
+          password: password.value,
+        }).then(response => {
+            console.log(response);
+        })
+      }
+
+      return { username, password, submitForm }
+    },
+  //   data() {
+  //     return {
+  //       username: '',
+  //       password: '',
+  //     }
+  //   },
+  //   methods: {
+  //     submitForm() {
+  //       // event.preventDefault(); // submit 시 새로고침 되는 form 의 기본 동작을 막음
+  //       const data = {
+  //         username: this.username,
+  //         password: this.password,
+  //       }
+
+  //       axios.post('https://jsonplaceholder.typicode.com/users', data)
+  //         .then(response => {
+  //           console.log(response);
+  //         })
+  //       console.log('제출됨');
+  //     }
+  //   }
+  }
+</script>
+
+<style scoped>
+
+</style>
